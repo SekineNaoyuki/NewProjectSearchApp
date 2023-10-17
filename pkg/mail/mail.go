5639225e3da8346a7b49774ef9d01884a6fd6e8a
EC2 Default User <ec2-user@ip-172-31-18-123.ap-southeast-2.compute.internal>
2023-10-17 14:02:38 +0000

    メールヘッダー設定

--- pkg/mail/mail.go
+++ pkg/mail/mail.go
@@ -6,9 +6,26 @@ import (
 )
 
 func SendEmail(subject, body string) error {
-	auth := smtp.PlainAuth("", constants.SmtpUsername, constants.SmtpPassword, constants.SmtpHost)
-	msg := []byte(body)
 
-	err := smtp.SendMail(constants.SmtpHost+":"+constants.SmtpPort, auth, constants.FromEmail, []string{constants.ToEmail}, msg)
+	headers := map[string]string{
+		"From": constants.FromEmail,
+		"To": constants.ToEmail,
+		"Subject": subject,
+		"MIME-Version": "1.0",
+		"Content-Type": "text/plain; charset=\"utf-8\"",
+	}
+
+	// ヘッダーを連結
+	var msgHeaders string
+	for key, value := range headers {
+		msgHeaders += key + ": " + value + "\r\n"
+	}
+
+	// メール本文とヘッダーを結合
+	message := msgHeaders + "\r\n" + body
+
+	auth := smtp.PlainAuth("", constants.SmtpUsername, constants.SmtpPassword, constants.SmtpHost)
+
+	err := smtp.SendMail(constants.SmtpHost+":"+constants.SmtpPort, auth, constants.FromEmail, []string{constants.ToEmail}, []byte(message))
 	return err
 }
